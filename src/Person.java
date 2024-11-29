03568b62dd6e45037aa19355656acace1245e310
w.kaiser@htl-grieskirchen.at <w.kaiser@htl-grieskirchen.at>
2024-11-29 17:24:44 +0100

    First commit

---
+++ src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    private String name;
+    private String firstnmae;
+
+    public Person(String name, String firstnmae) {
+        this.name = name;
+        this.firstnmae = firstnmae;
+    }
+}
