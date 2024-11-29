23b04eff3034a2cdfb43abfb6fa8b3439dd08693
w.kaiser@htl-grieskirchen.at <w.kaiser@htl-grieskirchen.at>
2024-11-29 17:34:41 +0100

    equals and hashCode

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Person {
     private String name;
     private String firstnmae;
@@ -5,5 +7,18 @@ public class Person {
     public Person(String name, String firstnmae) {
         this.name = name;
         this.firstnmae = firstnmae;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && Objects.equals(firstnmae, person.firstnmae);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, firstnmae);
     }
 }
